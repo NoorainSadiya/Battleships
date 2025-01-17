01238d3b2537507aaca058da6c7427bf05ac831c
NoorainSadiya <sadiya.noorain1@gmail.com>
2025-01-17 15:53:48 -0600

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/sadiyanoorain/Desktop/IITC/Sem3/MAD/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/sadiyanoorain/Desktop/IITC/Sem3/MAD/Modules/mp5-NoorainSadiya"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/sadiyanoorain/Desktop/IITC/Sem3/MAD/Modules/mp5-NoorainSadiya/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.1.0"
+export "FLUTTER_BUILD_NUMBER=0.1.0"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/sadiyanoorain/Desktop/IITC/Sem3/MAD/Modules/mp5-NoorainSadiya/.dart_tool/package_config.json"
